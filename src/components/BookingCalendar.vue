b182185c4051e9cbdb490c3888168d3d85840715
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-02-27 17:05:23 +0100

    added navigation buttons and their functionaly and styles

--- src/components/BookingCalendar.vue
+++ src/components/BookingCalendar.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div class="reservation-buttons">
+      <button @click="moveWeek(-1)">&#8249;</button>
+      <button @click="moveWeek(1)">&#8250;</button>
+      <button @click="goToToday">Today</button>
+    </div>
     <table>
       <thead>
         <tr>
@@ -30,10 +35,7 @@ export default {
       return reservationData;
     },
     dates() {
-      const today = new Date();
-      const startOfWeek = new Date(today);
-      startOfWeek.setDate(today.getDate() - today.getDay());
-
+      const startOfWeek = new Date(this.currentWeekStart);
       const dates = [];
       for (let i = 0; i < 7; i++) {
         const date = new Date(startOfWeek);
@@ -41,7 +43,21 @@ export default {
         dates.push(date.toISOString().slice(0, 10));
       }
       return dates;
-    }
+    },
+    filteredApartments() {
+      const startOfWeek = new Date(this.dates[0]);
+      const endOfWeek = new Date(this.dates[this.dates.length - 1]);
+
+      return this.apartments.filter((apartment) => {
+        const start = new Date(apartment.start);
+        const end = new Date(apartment.end);
+
+        return start <= startOfWeek && end >= endOfWeek;
+      });
+    },
+    currentWeekStart() {
+      return this.$store.getters.getCurrentWeekStart;
+    },
   },
   methods: {
     getReservationsForDay(apartmentId, date) {
@@ -62,9 +78,17 @@ export default {
         width: `${durationInDays * 100}%`,
         marginLeft: `${dayIndex * 100}%`,
       };
-    }
-  }
-}
+    },
+    moveWeek(amount) {
+      const newDate = new Date(this.currentWeekStart);
+      newDate.setDate(newDate.getDate() + amount * 7);
+      this.$store.dispatch('updateCurrentWeekStart', newDate);
+    },
+    goToToday() {
+      this.$store.dispatch('updateCurrentWeekStart', new Date());
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -82,5 +106,24 @@ export default {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+}
+.reservation-buttons {
+  margin: 20px;
+  display: flex;
+  gap: 10px;
+}
+
+.reservation-buttons button{
+  background-color: #000;
+  font-size: 14px;
+  color: white;
+  padding: 4px 16px;
+  transition: all 0.3s;
+}
+
+.reservation-buttons button:hover{
+  color: #000;
+  background-color: white;
+  cursor: pointer;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    currentWeekStart: new Date(),
   },
   getters: {
+    getCurrentWeekStart: (state) => state.currentWeekStart,
   },
   mutations: {
+    setCurrentWeekStart: (state, newDate) => {
+      state.currentWeekStart = newDate;
+    },
   },
   actions: {
+    updateCurrentWeekStart: ({ commit }, newDate) => {
+      commit('setCurrentWeekStart', newDate);
+    },
   },
   modules: {
-  }
-})
+  },
+});
