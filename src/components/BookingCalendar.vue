310d4bf4ca037100de44f41454ad7496c4e7f71e
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2024-02-28 16:01:23 +0100

    changed table visualization to divs and added additional styles to make it looks like a calendar

--- src/components/BookingCalendar.vue
+++ src/components/BookingCalendar.vue
@@ -1,22 +1,32 @@
 <template>
   <div>
-    <div class="reservation-buttons">
+    <nav class="reservation-buttons">
       <button @click="moveWeek(-1)">&#8249;</button>
       <button @click="moveWeek(1)">&#8250;</button>
       <button @click="goToToday">Today</button>
-    </div>
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th v-for="date in dates" :key="date" :class="{ 'current-day': isCurrentDay(date) }">{{ date }}</th>
+    </nav>
 
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="apartment in filteredApartments" :key="apartment.id">
-          <td>{{ apartment.roomDetails.name }}</td>
-          <td v-for="date in dates" :key="date" class="reservation-cell">
+    <div class="calendar-container">
+      <div class="dates-container">
+        <div class="dates-header">
+          <div
+            v-for="date in dates"
+            :key="date"
+            :class="{ 'current-day': isCurrentDay(date) }"
+          >
+            {{ date }}
+          </div>
+        </div>
+      </div>
+
+      <div class="reservations-container">
+        <div
+          v-for="apartment in filteredApartments"
+          :key="apartment.id"
+          class="apartment"
+        >
+          <div class="apartment-name">{{ apartment.roomDetails.name }}</div>
+          <div v-for="date in dates" :key="date" class="reservation-cell">
             <div class="reservations-container">
               <div
                 v-for="reservation in getReservationsForDay(apartment.id, date)"
@@ -26,10 +36,10 @@
                 {{ reservation.name }}
               </div>
             </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,17 +52,17 @@ export default {
       return reservationData;
     },
     dates() {
-  const startOfWeek = new Date(this.currentWeekStart);
-  startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() + 1);
+      const startOfWeek = new Date(this.currentWeekStart);
+      startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() + 1);
 
-  const dates = [];
-  for (let i = 0; i < 7; i++) {
-    const date = new Date(startOfWeek);
-    date.setDate(startOfWeek.getDate() + i);
-    dates.push(date.toISOString().slice(0, 10));
-  }
-  return dates;
-},
+      const dates = [];
+      for (let i = 0; i < 7; i++) {
+        const date = new Date(startOfWeek);
+        date.setDate(startOfWeek.getDate() + i);
+        dates.push(date.toISOString().slice(0, 10));
+      }
+      return dates;
+    },
     filteredApartments() {
       const startOfWeek = new Date(this.dates[0]);
       const endOfWeek = new Date(this.dates[this.dates.length - 1]);
@@ -70,9 +80,9 @@ export default {
   },
   methods: {
     isCurrentDay(date) {
-    const currentDate = new Date();
-    return date === currentDate.toISOString().slice(0, 10);
-  },
+      const currentDate = new Date();
+      return date === currentDate.toISOString().slice(0, 10);
+    },
     getReservationsForDay(apartmentId, date) {
       const reservations = [];
       return reservations.filter((reservation) => {
@@ -93,9 +103,52 @@ export default {
 </script>
 
 <style scoped>
-.reservation-cell {
-  border: 1px solid #ccc;
+.calendar-container {
+  display: flex;
+  flex-direction: column;
   position: relative;
+}
+
+.dates-container {
+  position: sticky;
+  z-index: 1;
+  margin-left: 200px;
+}
+
+.dates-header {
+  display: flex;
+}
+
+.dates-header > div {
+  flex: 1;
+  border: 1px solid #ccc;
+  padding: 10px 20px;
+  text-align: center;
+}
+
+.reservations-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-x: auto;
+}
+
+.apartment {
+  display: flex;
+}
+
+.apartment-name {
+  width: 190px;
+  border: 1px solid #ccc;
+  padding: 40px 5px;
+}
+
+.reservation-cell {
+  flex: 1;
+  border: 1px solid #ccc;
+  padding: 40px 60px;
+  position: relative;
+  background-color: white;
 }
 
 .reservations-container {
@@ -110,6 +163,7 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
 }
+
 .reservation-buttons {
   margin: 20px;
   display: flex;
@@ -130,7 +184,7 @@ export default {
   cursor: pointer;
 }
 
-th.current-day {
-    background-color: lightgreen;
-  }
+.current-day {
+  background-color: lightgreen;
+}
 </style>
